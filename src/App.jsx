6caba5f14c87e2a8a4e171409cdc0554fdcc5e38
Ashish Shukla <akshkgd@gmail.com>
2024-06-09 23:48:12 +0530

    wd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home";
+import About from './pages/About'
+import Navbar from './components/Navbar'
+import Error from "./pages/Error";
+import Requests from "./pages/Requests";
+import UserDetails from "./pages/UserDetails";
+
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl text-center mt-24 font-black">Welcome to react!</h1>
-        
+
+      <Navbar/>
+
+
+       <Routes>
+        <Route path="/" element={<Home />} />  
+        <Route path="/about" element={<About />} />
+        <Route path="/requests" element={<Requests />} />  
+        <Route path="/:username" element={<UserDetails/>} />
+        <Route path="*" element={<Error/>} />
+      </Routes> 
     </>
   )
 }
